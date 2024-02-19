815e10950d64b80c8776cb44f14f4467adaf1531
louie888a <alexlouie@ymail.com>
2024-02-19 14:49:47 +0000

    add questions

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    name: "title",
+    type: "input",
+    message: "What is the title of your project?",
+  },
+  {
+    name: "description",
+    type: "input",
+    message: "Provide a description of the project.",
+  },
+  {
+    name: "installation",
+    type: "input",
+    message: "Provide instructions for installation.",
+  },
+  {
+    name: "usage",
+    type: "input",
+    message: "Provide instructions for usage.",
+  },
+  {
+    name: "contribution",
+    type: "input",
+    message: "Provide instructions for contribution.",
+  },
+  {
+    name: "test",
+    type: "input",
+    message: "Provide instructions for testing.",
+  },
+  {
+    name: "license",
+    type: "list",
+    message: "Choose a license for your project.",
+    choices: ["MIT", "Apache", "GPL", "None"],
+  },
+  {
+    name: "github",
+    type: "input",
+    message: "Enter your GitHub username.",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "Enter your email address.",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
