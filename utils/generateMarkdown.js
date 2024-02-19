12d16aac5b7bad4d96a2ee95392f15f1e28171db
louie888a <alexlouie@ymail.com>
2024-02-19 15:21:07 +0000

    filled data to function "GenerateMarkdown"

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![![License](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test](#test)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## License
+  This project is licensed under the ${data.license} license.
+
+  ## Questions
+  If you have any questions, please reach out to me at: ${data.email}
+  GitHub: https://github.com/${data.github}
 `;
 }
 
